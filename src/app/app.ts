62c71711acb3700198b521086ac0eb578128cf23
UrosR10 <urosmiloradovic2000@gmail.com>
2026-03-04 21:20:59 +0100

    Rad sa casa , home.ts , flightModel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('KVA-projekat1');
+  ime = 'Uros'
+  prezime = 'Milorad'
+  indeks = '2023203815'
 }
---
+++ src/models/flight.model.ts
@@ -0,0 +1,20 @@
+export interface FlightModel {
+
+    id: 251108
+    type: {
+        id: number
+        name: string
+    }
+    flightKey : string
+    flightNumber: string
+    destination: string
+    scheduledAt: string
+    estimatedAt: null | string
+    connectedType: string
+    connectedFlight: string
+    plane: string
+    gate: null | string
+    terminal: number
+}
+
+
