c51c8d10086a70dc492ef59acc2a8831d0089694
UrosR10 <urosmiloradovic2000@gmail.com>
2026-03-05 19:51:30 +0100

    Uradjen home(cards) , ikonice ubacene , loading

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,19 @@ import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref ,MatButtonModule , MatMenuModule, MatToolbarModule ],
+  imports: [
+    RouterOutlet,
+    RouterLinkWithHref ,
+    MatButtonModule ,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,3 +25,4 @@ export class App {
   prezime = 'Milorad'
   indeks = '2023203815'
 }
+
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,19 +2,28 @@ import { Component, signal } from '@angular/core';
 import axios from 'axios';
 import { FlightModel } from '../../models/flight.model';
 import { RouterLink } from "@angular/router";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { Utils } from '../utils';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, MatCardModule , MatButtonModule , MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   flights = signal<FlightModel[]>([])
 
-  constructor(){
-    axios.get('https://flight.pequla.com/api/flight/list?type=departure')
-    .then(rsp => this.flights.set(rsp.data))
-  }
+  constructor(public utils: Utils){
+    axios.get<FlightModel[]>('https://flight.pequla.com/api/flight/list?type=departure')
+    .then(rsp => {
+
+      const sorted = rsp.data.sort((a1,a2)=> new Date(a1.scheduledAt).getTime() - new Date(a2.scheduledAt).getTime())
+      this.flights.set(rsp.data)
+  })
+
+}
 
 }
